5f1a50b6734ac678dddc3da7465551912e111fda
Raju <rajutamang3456@gmail.com>
2024-10-04 08:46:56 +0545

    title and  meta

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -1,4 +1,28 @@
-function About() {
-    return <h4>this is about us page</h4>
-}
- export default About
+
+// import Head from "next/head"
+
+// function About() {
+   
+//     return <> 
+//      <Head>
+//         <title>about raju tamang</title>
+//         <meta name="description" content="want to know about raju music"></meta>
+//      </Head>
+//     <h4>this is about us page</h4>
+//     </>
+// }
+//  export default About
+import type { Metadata } from "next";
+
+export const metadata: Metadata = {
+    title: "abouit Next App",
+    description: "what is this",
+  };
+export default function About() {
+    return (
+      <>
+      
+        <h4>This is the about us page</h4>
+      </>
+    );
+  }
--- app/page.tsx
+++ app/page.tsx
@@ -2,9 +2,9 @@ import Link from "next/link";
 
 export default function Home() {
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
+    <div className="bg-gray-500 grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
       <h4>hello world</h4>
-      <Link href={"/blog"}>
+      <Link className="text-red-500" href={"/blog"}>
         blog
       </Link>
       <h1 className="text-3xl font-bold underline">hello nepal</h1>
